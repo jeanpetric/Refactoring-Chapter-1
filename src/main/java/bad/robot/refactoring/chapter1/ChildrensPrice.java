47638d389382f35bb3a974567e38ca2f4ed15729
Jean Petric <petricomp@gmail.com>
2016-11-22 17:05:03 +0000

    making small modification

--- src/main/java/bad/robot/refactoring/chapter1/ChildrensPrice.java
+++ src/main/java/bad/robot/refactoring/chapter1/ChildrensPrice.java
@@ -10,7 +10,7 @@ public class ChildrensPrice extends Price {
     @Override
     double getCharge(int daysRented) {
         double amount = 1.5;
-        if (daysRented > 3)
+        if (daysRented > 4)
             amount += (daysRented - 3) * 1.5;
         return amount;
     }
